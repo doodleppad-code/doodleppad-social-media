2bddf9b84c251a331999dc9b2428f9ed70730344
unknown <adityamhsohagpure999@gmail.com>
2025-12-13 20:46:04 +0530

    test2

--- App.js
+++ App.js
@@ -33,81 +33,8 @@ function RootNavigator() {
 
 export default function App() {
   return (
-<<<<<<< HEAD
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-
-      <Stack.Screen
-
-        name="Login"
-
-        component={Login}
-
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="PhoneAuth"
-        component={PhoneAuth}
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="Otp"
-        component={Otp}
-        options={{ headerShown: false }} /> 
-      <Stack.Screen
-        name="Detail"
-        component={Details}
-        options={{ headerShown: false }} />
-
-      <Stack.Screen
-        name="Dashboard"
-        component={Dashboard}
-        options={{ headerShown: false }} />
-
-      <Stack.Screen
-        name="ChatList"
-        component={ChatList}
-        options={{ headerShown: false }} />
-
-      <Stack.Screen
-        name="ChatScreen"
-        component={ChatScreen}
-        options={{ headerShown: false }} />
-        
-      <Stack.Screen
-        name="SignUp"
-        component={SignUp}
-        options={{ headerShown: false }} />
-      <Stack.Screen
-        name="Doodlepad"
-        component={DoodlePad}
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="SearchScreen"
-        component={SearchScreen}
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="NotificationScreen"
-        component={NotificationScreen}
-        options={{ headerShown: false }} />
-        <Stack.Screen
-        name="ProfileUI"
-        component={ProfileUI}
-     options={{ headerShown: false }}/>
-     <Stack.Screen
-      name="ImagePost"
-      component={Imagepost}
-        options={{ headerShown: false }} />
-    </Stack.Navigator>
-      </NavigationContainer>
-    </GestureHandlerRootView>
-=======
     <AuthProvider>
       <RootNavigator />
     </AuthProvider>
->>>>>>> af138d7b188736ea574fdb0dec19a2d413c46c12
   );
 }
